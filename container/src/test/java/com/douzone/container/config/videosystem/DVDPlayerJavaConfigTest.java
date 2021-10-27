6a390142c4aa67d105bd6bb874d09b9509034a59
sysyo <youngiya77@gmail.com>
2021-10-27 16:18:06 +0900

    Explicit - Java #1

--- container/src/test/java/com/douzone/container/config/videosystem/DVDPlayerJavaConfigTest.java
+++ container/src/test/java/com/douzone/container/config/videosystem/DVDPlayerJavaConfigTest.java
@@ -1,6 +1,5 @@
 package com.douzone.container.config.videosystem;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 import org.junit.Rule;
@@ -11,27 +10,29 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import com.douzone.container.soundsystem.CDPlayer;
+import com.douzone.container.videosystem.DVDPlayer;
+import com.douzone.container.videosystem.DigitalVideoDisc;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations={"DVDPlayerConfig.class"})
+@ContextConfiguration(classes={DVDPlayerConfig.class})
 public class DVDPlayerJavaConfigTest {
-
 	@Rule
 	public final SystemOutRule systemOutRule = new SystemOutRule().enableLog();
 	
 	@Autowired
-	private CDPlayer cdplayer;
-	
+	private DigitalVideoDisc dvd;
+
+	@Autowired
+	private DVDPlayer dvdplayer;
+
 	@Test
-	public void testCDPlayerNotNull() {
-		assertNotNull(cdplayer);
+	public void testDVDNotNull() {
+		assertNotNull(dvd);
 	}
 	
 	@Test
-	public void testPlay() {
-		cdplayer.play();
-		assertEquals("Playing 붕붕 by 김하온", systemOutRule.getLog().replace("\r\n", "").replace("\n", ""));
+	public void testDVDPlayerNotNull() {
+		assertNotNull(dvdplayer);
 	}
 	
 }

f79400b8bc1da9a75cddd272ff72689d36b8a226
sysyo <youngiya77@gmail.com>
2021-10-27 11:55:13 +0900

    auto - java config

---
+++ container/src/main/java/com/douzone/container/config/soundsystem/CDPlayerConfig.java
@@ -0,0 +1,10 @@
+package com.douzone.container.config.soundsystem;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = {"com.douznoe.container.soundsystem"})
+public class CDPlayerConfig {
+
+}
